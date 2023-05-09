5ae36fa391d5272bbaba4de2379cef36781046d1
liukuikun <641417025@qq.com>
2023-05-09 10:55:46 +0800

    update requirement

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import torch
 from PIL import Image
 
 from mmgpt.models.builder import create_model_and_transforms
+from openxlab.model import download
 
 TEMPLATE = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
 response_split = "### Response:"
@@ -356,6 +357,28 @@ def build_conversation_demo():
 
 
 if __name__ == "__main__":
+    # download('houshaowei/Multimodal-GPT', 'mmgpt-lora-v0-release',
+    #          './checkpoints/')
+    # download('houshaowei/Multimodal-GPT', 'pytorch_model-00001-of-00002',
+    #          './checkpoints/llama-7b_hf/')
+    download('houshaowei/Multimodal-GPT', 'pytorch_model-00002-of-00002',
+             'checkpoints/llama-7b_hf/')
+    # download('houshaowei/Multimodal-GPT', 'tokenizer',
+    #          './checkpoints/llama-7b_hf/')
+    # download('houshaowei/Multimodal-GPT', 'tokenizer_config',
+    #          './checkpoints/llama-7b_hf/')
+    # download('houshaowei/Multimodal-GPT', 'special_tokens_map',
+    #          './checkpoints/llama-7b_hf/')
+    # openxlab pytorch_model.bin.index.json->pytorch_model_bin_index.json
+    download('houshaowei/Multimodal-GPT', 'pytorch_model_bin_index',
+             'checkpoints/llama-7b_hf/')
+    # download('houshaowei/Multimodal-GPT', 'generation_config',
+    #          './checkpoints/llama-7b_hf/')
+    # download('houshaowei/Multimodal-GPT', 'config',
+    #          './checkpoints/llama-7b_hf/')
+    # download('houshaowei/Multimodal-GPT', 'checkpoint',
+    #          './checkpoints/OpenFlamingo-9B/')
+
     llama_path = "checkpoints/llama-7b_hf"
     open_flamingo_path = "checkpoints/OpenFlamingo-9B/checkpoint.pt"
     finetune_path = "checkpoints/mmgpt-lora-v0-release.pt"
@@ -367,6 +390,6 @@ if __name__ == "__main__":
     init_memory = str(round(torch.cuda.memory_allocated() / 1024**3, 2)) + 'GB'
     demo = build_conversation_demo()
     demo.queue(concurrency_count=3)
-    IP = "0.0.0.0"
-    PORT = 8997
-    demo.launch(server_name=IP, server_port=PORT, share=True)
+    # IP = "0.0.0.0"
+    # PORT = 8997
+    demo.launch(share=True)
